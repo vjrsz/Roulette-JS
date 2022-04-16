a61ede11e08ec8c48259a6cbc7829b8632de0540
vjrsz <juniornunes1008@gmail.com>
2022-04-16 11:13:40 -0300

    first commit

--- main.js
+++ main.js
@@ -2,12 +2,21 @@ const SCREEN = { w : 720, h: 480 }
 
 const game = {
     id : document.getElementById("game"),
-    itens: 7,
-    settings: {
-        reels : {
-            size : {  w: 720, h: 480 },
-            itens_size : { w: 80, h: 80 },
-        }
+    itens: 18,
+    reels : {
+        size : {  w: 720, h: 480 },
+        itens_size : { w: 80, h: 80 },
+            
+    },
+    colors: {
+        white: "#ebdfcc",
+        black: "#021b2b",
+        red: "#ff0841",
+    },
+    rgb: {
+        white: "rgb(235 223 204)",
+        black: "rgb(2 27 43)",
+        red: "rgb(255 8 65)",
     }
 }
 const slotMachine = {
@@ -24,90 +33,123 @@ const slotMachine = {
     },
     swap : function(){
         setTimeout(()=>{
-            game.reels.reels_r.className = "anim"
+            game.reels.reels_row.className = "anim"
+            swap_itens()
         }, 1000)
         setTimeout(()=>{
-            game.reels.reels_r.className = ""
+            game.reels.reels_row.className = ""
+            win()
         }, 4000)
     }
 }
 function createReels(){
 	let reels = document.createElement("div")
-    let reels_r = document.createElement("div")
+    let reels_row = document.createElement("div")
+    let reels_line = document.createElement("div")
+    let line = document.createElement("div")
     let reels_itens = []
 
     /** REELS */
     reels.id = "reels"
-    reels_r.id = "reels-r"
+    reels_row.id = "reels-row"
+    reels_line.id = "reels-line"
 
-    styleDiv(reels,  game.settings.reels.size.w,  game.settings.reels.size.h)
+    styleDiv(reels,  game.reels.size.w,  game.reels.size.h)
+    styleDiv(reels_line, game.reels.size.w,  game.reels.size.h)
+    styleDiv(line, (game.reels.itens_size.w/20), game.reels.itens_size.h*2)
+    reels_line.appendChild(line)
+
+    let itens;
+    let id;
 
     /** Row */
     for (var j = 1; j <= game.itens; j++) {
-        /**Reds*/
-        let itens = document.createElement("div")
-        let id = 'reels'+j
-
-        styleDiv(itens, game.settings.reels.itens_size.w, game.settings.reels.itens_size.h)
-        itens.id = id
-        itens.style.background = "#ff0841"
-        itens.innerHTML = "<p>"+j+"</p>"
-        reels_itens.push(itens)
-        reels_r.appendChild(itens)
-
         /**White*/
-        if( j == 4 ){
+        if( j == ((game.itens)/2)+1 ){
             itens = document.createElement("div")
-            id = 'reels'+(15-j)
+            id = 'reelsC'
 
-            styleDiv(itens, game.settings.reels.itens_size.w, game.settings.reels.itens_size.h)
+            styleDiv(itens, game.reels.itens_size.w, game.reels.itens_size.h)
             itens.id = id
-            itens.style.background = "#ebdfcc"
+            itens.style.background = game.colors.white
             itens.innerHTML = "<p>C</p>"
 
             reels_itens.push(itens)
-            reels_r.appendChild(itens)
+            reels_row.appendChild(itens)
         }
-        /**Blacks*/
-        itens = document.createElement("div")
-        id = 'reels'+(15-j)
 
-        styleDiv(itens, game.settings.reels.itens_size.w, game.settings.reels.itens_size.h)
+        /**Reds*/
+        itens = document.createElement("div")
+        id = 'reels'+j
+
+        styleDiv(itens, game.reels.itens_size.w, game.reels.itens_size.h)
         itens.id = id
-        itens.style.background = "#021b2b"
-        itens.innerHTML = "<p>"+(15-j)+"</p>"
+        itens.style.background = game.colors.red
+        itens.innerHTML = "<p>"+j+"</p>"
+        reels_itens.push(itens)
+        reels_row.appendChild(itens)
+
+        /**Blacks*/
+        let id_number = (game.itens*2)+1-j
+        itens = document.createElement("div")
+        id = 'reels'+(id_number)
+
+        styleDiv(itens, game.reels.itens_size.w, game.reels.itens_size.h)
+        itens.id = id
+        itens.style.background = game.colors.black
+        itens.innerHTML = "<p>"+(id_number)+"</p>"
 
         reels_itens.push(itens)
-        reels_r.appendChild(itens)
+        reels_row.appendChild(itens)
     }
 
-
-    reels.appendChild(reels_r)
+    reels.appendChild(reels_line)
+    reels.appendChild(reels_row)
     game.id.appendChild(reels)
     game.reels = {
         id : reels,
-        reels_r : reels_r,  
+        reels_row : reels_row,  
         reels_itens : reels_itens
     } 
-}
-function swap_itens(){
-    let max = 14
-    let min = 1
-    move = Math.trunc(Math.random() * (max - min) + min)
-	
-    /*
-        swaps = document.createElement("span")
-
-        reels_img[img_1].parentNode.insertBefore(swaps, reels_img[img_1])
-        reels_img[img_2].parentNode.insertBefore(reels_img[img_1], reels_img[img_2])
-        swaps.parentNode.insertBefore(reels_img[img_2], swaps)
-        swaps.remove()
-    */
 }
 function styleDiv(div, width, height){
     div.style.width = width+'px';
     div.style.height = height+'px';
 }
+function swap_itens(){
+    let max = game.itens*2
+    let min = 1
+    let reels_row = game.reels.reels_row
+    let move = Math.trunc(Math.random() * (max - min) + min)
+
+	for (var i = 0; i < move; i++) {
+        reels_row.firstChild.parentNode.insertBefore(reels_row.lastChild, reels_row.firstChild)
+
+        //console.log(reels_row.lastChild, reels_row.firstChild, move)
+    }
+}
+function win(){
+    let pos = game.reels.reels_row.childNodes[game.itens] 
+    let item_win = {
+        number : pos.firstChild.innerHTML,
+        color : ''
+    }
+
+    /** rgbToHex */
+    let color = pos.style.background.replace(',', '').replace(',', '')
+
+    /** colors */
+    if(color == game.rgb.white){
+        item_win.color = "white"
+    }else if(color == game.rgb.red){
+        item_win.color = "red"
+    }else if(color == game.rgb.black){
+        item_win.color = "black"
+    }
+
+    console.log(item_win)    
+}
+
 slotMachine.init()
 
 /*
